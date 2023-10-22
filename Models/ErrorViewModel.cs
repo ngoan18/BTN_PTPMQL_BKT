1a7359d4e735f6e7b1d174a96ea2d87307320d94
ngoan18 <Buikimngoan240@gmail.com>
2023-10-22 18:29:00 +0700

    first commit

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace BTN_PTPMQL_BKT.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
